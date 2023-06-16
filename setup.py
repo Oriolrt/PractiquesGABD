e4729a01a87261580ab2a4aeed6f3f39d064d341
Oriol Ramos Terrades <oriol.ramos@uab.cat>
2023-06-16 17:16:33 +0200

    - Modificat fitxer de setup

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
