057aa27a2da1da2859318cf38e34fed491f86687
Oriol Ramos Terrades <38754866+Oriolrt@users.noreply.github.com>
2023-06-16 15:45:55 +0200

    Create oracleConnection.py

---
+++ src/oracleConnection.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+u'''
+Created on Jun 12, 2018
+
+@author: oriolrt
+'''
+
+import glob
+import numpy as np
+import os
+import re
+import struct
+
+from getpass import getpass
+import logging
+import pandas as pd
+import oracleDB
+from sshtunnel import SSHTunnelForwarder
+
+
+
+import AbsConnection as AC
+import utils as u
+
+
+
+class oracleConnection(AC.AbsConnection):
+  '''
+    classdocs
+    '''
+
+  __slots__ = ['__cursor','__serviceName']
+
+  def __init__(self, **params):
+    '''
+        Constructor
+        '''
+    super(oracleConnection, self).__init__(**params)
+    self.__serviceName=params.pop('serviceName','orcl')
+
+    self.__cursor = None
+
+  def cursor(self):
+    try:
+      self.__cursor.callproc("dbms_output.enable")
+      return self.__cursor
+    except cx_Oracle.DatabaseError:
+      logging.warning('Database connection already closed')
+
+
+
+  def open(self):
+    """
+      Connect to a oracle server given the connexion information saved on the cfg member variable.
+
+      :return: None
+    """
+
+    ssh_data = self.ssh
+
+    if self.pwd is None:
+      self.pwd = getpass(prompt="Password de l'usuari {} d'Oracle: ".format(self.user))
+
+    if ssh_data is not None:
+      if "pwd" in ssh_data:
+        if ssh_data["pwd"] == "" or ssh_data["pwd"] is None:
+          ssh_data["pwd"] = getpass(prompt="Password de l'usuari {} a {}: ".format(ssh_data["user"], ssh_data["ssh"]))
+      else:
+        ssh_data["pwd"] = getpass(prompt="Password de l'usuari {} a {}: ".format(ssh_data["user"], ssh_data["ssh"]))
+
+      DSN = "{}/{}@localhost:{}/{}".format(self.user, self.pwd, ssh_data['port'], self.__serviceName)
+
+      self.server = SSHTunnelForwarder((ssh_data["ssh"], int(ssh_data['port'])),
+                                       ssh_username=ssh_data["user"],
+                                       ssh_password=ssh_data["pwd"],
+                                       remote_bind_address=(self.hostname, int(self.port)),
+                                       local_bind_address=("", int(ssh_data['port']))
+                                       )
+
+      self.server.start()
+
+
+    else:
+      DSN = "{}/{}@{}:{}/{}".format(self.user, self.pwd, self.hostname, self.port,self.__serviceName)
+
+    self.conn = cx_Oracle.connect(DSN)
+    self.__cursor = self.conn.cursor()
+
+
+
+  def close(self):
+    try:
+      self.conn.close()
+      if self.server is not None:  self.server.stop()
+    except cx_Oracle.DatabaseError:
+      logging.warning('Database connection already closed')
+
+
+
+  def commit(self):
+    self.conn.commit()
+
+  def testConnection(self):
+    cur = self.__cursor
+
+    res = cur.execute("""SELECT sys_context('USERENV','SESSION_USER')  as "CURRENT USER" ,
+                      sys_context('USERENV', 'CURRENT_SCHEMA') as "CURRENT SCHEMA"
+                      FROM dual""").fetchone()
+
+    print("Current user: {}, Current schema: {}".format(res[0], res[1]))
+
+    return True
+
+  def startSession(self):
+    self.connectDB
+    self._noConnection__isStarted = True
+    return True
+
+  def _showMessages(self):
+
+    statusVar = self.cursor.var(cx_Oracle.NUMBER)
+    lineVar = self.cursor.var(cx_Oracle.STRING)
+    while True:
+      self.cursor.callproc("dbms_output.get_line", (lineVar, statusVar))
+      if statusVar.getvalue() != 0:
+        break
+      print(lineVar.getvalue())
